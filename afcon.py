278dd859db43db28b66ae1eacd18c146b1959e9e
Mmeli Ntshakala <mmeliroot@gmail.com>
2025-12-23 15:31:45 +0200

    link printed

--- afcon.py
+++ afcon.py
@@ -2,11 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 
 link = "https://www.transfermarkt.com"
-afcon_link = f"{BASE_URL}/africa-cup-of-nations/startseite/pokalwettbewerb/AFCN?saison_id=2024"
+afcon_link = f"{link}/africa-cup-of-nations/startseite/pokalwettbewerb/AFCN?saison_id=2024"
+
+# Headers to mimic browser
 
 HEADERS = { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " 
                 "AppleWebKit/537.36 (KHTML, like Gecko) " 
                 "Chrome/120.0.0.0 Safari/537.36" 
 }
 
+# GET TEAMS 
+
 response = requests.get(afcon_link, headers=HEADERS)
+soup = BeautifulSoup(response.text, "lxml")
+
+print(soup)
