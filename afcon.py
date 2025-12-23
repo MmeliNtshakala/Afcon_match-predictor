2e2547797eab59fcb786619388d1d590f6453bcb
Mmeli Ntshakala <mmeliroot@gmail.com>
2025-12-23 14:50:19 +0200

    changes and header

--- afcon.py
+++ afcon.py
@@ -1,1 +1,12 @@
 import requests
+from bs4 import BeautifulSoup
+
+link = "https://www.transfermarkt.com"
+afcon_link = f"{BASE_URL}/africa-cup-of-nations/startseite/pokalwettbewerb/AFCN?saison_id=2024"
+
+HEADERS = { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " 
+                "AppleWebKit/537.36 (KHTML, like Gecko) " 
+                "Chrome/120.0.0.0 Safari/537.36" 
+}
+
+response = requests.get(afcon_link, headers=HEADERS)
